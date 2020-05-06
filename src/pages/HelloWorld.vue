fb71c335148812c18873f1a961969bf568684897
Marton Lederer <martondeveloper@gmail.com>
2020-05-06 16:00:31 +0200

    Base

--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
@@ -81,33 +82,54 @@
       </li>
     </ul>
   </div>
+
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+
+  export default {
+
+    name: 'HelloWorld',
+    data () {
+
+      return {
+
+        msg: 'Welcome to Your Vue.js App'
+
+      }
+
     }
+
   }
-}
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+  h1, h2 {
+
+    font-weight: normal;
+
+  }
+
+  ul {
+
+    list-style-type: none;
+    padding: 0;
+
+  }
+
+  li {
+
+    display: inline-block;
+    margin: 0 10px;
+
+  }
+
+  a {
+
+    color: #42b983;
+
+  }
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import NotFound from '@/pages/NotFound'
+import HelloWorld from '@/pages/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+
   routes: [
+
     {
+
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+
+    },
+    
+    //404 routes. Must be at the end of the routes array
+    {
+
+      path: '/404',
+      name: 'NotFound',
+      component: NotFound
+
+    },
+    {
+
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
+
     }
+
   ]
+
 })
