8f152e7f0d112e9ca8e3531afb0781ed65e0fd6a
Marton Lederer <martondeveloper@gmail.com>
2020-06-01 22:52:05 +0200

    Added lodash templates based head config

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,21 @@ module.exports = {
 
     }
 
+  },
+  chainWebpack: (config) => {
+
+    config
+      .plugin('html')
+      .tap((args) => {
+
+        args[0].title = 'Vue CLI starter kit'
+        args[0].description = 'Vue CLI starter kit description'
+        args[0].themeColor = '#ff0000'
+
+        return args
+
+      })
+
   }
 
 }
